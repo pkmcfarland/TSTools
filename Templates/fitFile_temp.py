09e1407decdec28be7a7562bd64c93289b9ac953
twistmcflip <p.k.mcfarla@gmail.com>
2019-07-27 15:19:23 -0400

    Updated fitFile_temp.py to match current program design

--- Templates/fitFile_temp.py
+++ Templates/fitFile_temp.py
@@ -1,22 +1,29 @@
-# all blank lines and all lines starting with # will be skipped
-# use 1 (on) and 0 (off) to toggle functionality
+
+# General rules for constructing file:
+# 1.) all blank lines and all lines starting with # will be skipped
+# 2.) line must start with 2-char flag followed by colon
+# 3.) all flag args must be on same line as flag
+# 4.) flags can be UPPER or lower 
+# 5.) white space separates flag from args, can have as much white
+#     space as you like
+
+# Rules for parameter est flags:
+# 6.) use 1 (on) and 0 (off) to toggle functionality
 
 # Flags for type of inversion to perform:
 
-IM: basin           # inversion method: linear = weighted least squares
-                    #                   basin = basinhopping (must also include LM flat)
+IM: basin           # (I)nversion (M)ethod: linear = weighted least squares
+                    #                       basin = basinhopping (must also include LM flat)
 
-LM: nelder          # local-minimum finder: nelder = Nelder-Mead
+LM: nelder          # (L)ocal-(M)inimum finder: nelder = Nelder-Mead
 
-# Flags for parameters to estimate for components: x1 x2 x3
+# Flags for parameters to estimate for components:
+# Note - if coord.s in dEdNdU x1 = E, x2 = N, x3 = U
+#        if coord.s in dXdYdZ x1 = X, x2 = Y, x3 = Z
 
-POS: 1 1 1          # position at t_0
-VEL: 1 1 1          # linear velocity
-AN: 1 1 1           # seasonal signal with annual period
-SA: 1 1 1           # seasonal signal with semi-annual period
-
-# Flag for break information file
-
-BF: ./earthQuakes.txt       # path and file name of break file can be
-                            # provided multiple times to include multiple files
-BF: ./equipmentChanges.txt
+#   x1  x2  x3
+DC:  1   1   1      # (DC)-offset term (i.e. pos at t=0)
+VE:  1   1   1      # linear (VE)locity
+AN:  1   1   1      # seasonal signal with (AN)nual period
+SA:  1   1   1      # seasonal signal with (S)emi-(A)nnual period
+PO:  0   0   0      # 4th order (PO)lynomial **NOT YET FUNCTIONAL**
