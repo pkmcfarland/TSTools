70d292b6d1e6e5d9da2d2db5070493a34ec1cc68
twistmcflip <p.k.mcfarla@gmail.com>
2019-08-25 16:30:54 -0400

    modified fitFile_temp.cmd so that there are individual flags for sine and cosines for annual and semi-annual seasonals. Also half-started genEqnMatrices but decided to quit this effort for now.

--- python/src/tstools/genEqnMatrices.py
+++ python/src/tstools/genEqnMatrices.py
@@ -9,12 +9,31 @@ Generate the matrices needed to either:
 import numpy as np
 
 ########################################################################
-def genMtxFromFiles(decYearList, fitFile, brkFile):
+def genMtxFromInFiles(decYearList, fitFile, brkFile):
 
     """
     Generate kinematic equation matrices directly from FitFile and 
     BreakFile objects. Usually used to generate synthetic time
     series.
     """
+    
+    # check that the input fitFile was set to gensyn
+    if fitFile.im != 'gensyn':
+        
+        print(f"ERROR in {fitFile.name}, IM must be set to 'gensyn' to"
+             +f" use with genMtxFromInFiles()")
+             return -1
+    else:
 
-    x1FitFileParams = [fitFile.dc
+        # get reference epoch
+        refYear = fitFile.re
+        
+        # get parameters for each component from fitFile
+        x1FitFileParams = [fitFile.dc[0], fitFile.ve[0], fitFile.sa[0],
+                           fitFile.cs[0], fitFile.ss[0], fitFile.cs[0]]
+        x2FitFileParams = [fitFile.dc[1], fitFile.ve[1], fitFile.sa[1],
+                           fitFile.cs[1], fitFile.ss[1], fitFile.cs[1]]
+        x3FitFileParams = [fitFile.dc[2], fitFile.ve[2], fitFile.sa[2],
+                           fitFile.cs[2], fitFile.ss[2], fitFile.cs[2]]
+        
+        
