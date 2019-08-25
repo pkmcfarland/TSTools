220cad7fd3464b12bfe584860c4ab612f3718d68
twistmcflip <p.k.mcfarla@gmail.com>
2019-08-25 15:00:50 -0400

    created new module for generating kinematic equation matrices

---
+++ python/src/tstools/genEqnMatrices.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Generate the matrices needed to either:
+1.) create synthetic time series,
+2.) perform least squares estimation of kinematic equation parameters, or
+3.) compute residuals when using a non-linear solver.
+"""
+import numpy as np
+
+########################################################################
+def genEqnMatices(decYearList, fitFileObj, brkFileObj):
+
+    """
+    Generate the matrices needed to create synthetic time series or
+    invert time series for kinematic equation parameters.
+    """
+
+    pass
