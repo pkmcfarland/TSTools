545fa827cf460991f131bd23d0f7241db99e3aa5
twistmcflip <p.k.mcfarla@gmail.com>
2019-11-08 15:58:40 -0500

    added functions to UnrBreakFile class to read in file contents from web, read from local, and write local

--- python/src/tstools/unrFiles.py
+++ python/src/tstools/unrFiles.py
@@ -28,20 +28,81 @@ class UnrBreakFile:
         self.lineList = []
 
     ####################################################################
-    def fetch(self, url=''):
+    def fetch(self, url='', site=''):
 
         """
         Read UNR break file from UNR website into UnrBreakFile object.
         """
         
+        # clear self.lineList from possible previous file reads
+        self.lineList = []
+        
         # set download url if not set by user
-        if url == '':
-            url = self.url
+        if url != '':
+            self.url = url
+
+        # set site to all if not set by user
+        if site != '':
+            self.site = site
 
         # download file
         raw = req.get(url)
 
         # split lines into list elements
         for line in raw.text.split('\n'):
-            self.lineList.append(line)
+            # if self.site not 'all' only pull out the lines
+            # for the site of interest
+            if self.site == ALL:
+                self.lineList.append(line)
+            else:
+                if line.split()[0] == self.site:
+                    self.lineList.append(line)
+    
+    ####################################################################
+    def readLocal(self, fileName, site=''):
+
+        """
+        Read local UNR break file into UnrBreakFile object.
+        """
+        
+        # clear self.lineList from possible previous file reads
+        self.lineList = []
+
+        if site != '':
+            self.site = site
+        else:
+            self.site = ALL
+
+        if self.site == ALL:
+            with open(fileName,'r') as rf:
+                for line in rf:
+                    self.lineList.append(line.strip('\n'))
+
+        else:
+            with open(fileName,'r') as rf:
+                for line in rf:
+                    if line.split()[0] == self.site:
+                        self.lineList.append(line.strip('\n'))
+
+    ####################################################################
+    def write(self, fileName, site=''):
+    
+        """
+        Write contents of UnrBreakFile object out to text file.
+        """
+        
+        if site != '':
+            self.site = site
+        else:
+            self.site = ALL
+
+        wf = open(fileName,'w')
+        if self.site == ALL:
+            for line in self.lineList:
+                wf.write(line+'\n')
+        else:
+            for line in self.lineList:
+                if line.split()[0] == self.site:
+                    wf.write(line+'\n')
+
 
